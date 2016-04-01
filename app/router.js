6c387bbf6d5152f92c6be5f834266ed571399bdc
Megan <mefayer@gmail.com>
2016-04-01 08:36:59 -0700

    add top nav bar on every page

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('contact');
+  this.route('question');
+  this.route('answer');
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'ask',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://askquestion.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
