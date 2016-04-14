a681da7cc934a5ec741815ce4dbf83a6a93c6c66
Megan <mefayer@gmail.com>
2016-04-14 09:58:31 -0700

    fix everything but the upvotes

--- app/routes/category.js
+++ app/routes/category.js
@@ -1,12 +1,12 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  bookmarkQuestions: Ember.inject.service(),
-
-  model(params) {
-    return this.store.query('question', {
-      orderBy: 'category',
-      equalTo: 'question.params'
-    });
-  }
-});
+// import Ember from 'ember';
+//
+// export default Ember.Route.extend({
+//   bookmarkQuestions: Ember.inject.service(),
+//
+//   model(params) {
+//     return this.store.query('question', {
+//       orderBy: 'category',
+//       equalTo: 'question.' + params
+//     });
+//   }
+// });
